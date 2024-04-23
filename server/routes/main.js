9af6566c25b63facf37249961a280815ecb74a7a
cyponos <98052408+cyponos@users.noreply.github.com>
2024-04-23 15:18:31 -0500

    Main

---
+++ server/routes/main.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const router = express.Router();
+
+router.get('', (req,res) => {
+    const locals = {
+        title: "Tweddit",
+        description: "Simple Blog created with NodeJs, Express & MongoDb"
+    }
+    
+    
+    
+    
+    
+    res.render('index', locals);
+});
+
+router.get('/about', (req,res) => {
+    res.render('about');
+});
+
+module.exports = router;
